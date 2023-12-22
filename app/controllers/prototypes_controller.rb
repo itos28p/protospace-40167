8d4ac1d0ae86777e375e323c9522a82d8ea8425b
itos28p <rstoyo.sayaka.ito@gmail.com>
2023-12-22 14:47:24 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,11 +3,26 @@ class PrototypesController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @prototypes = Prototype.all
   end
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
+    end
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = current_user.prototypes.build(prototype_params)
+    
+    if @prototype.save
+      redirect_to prototypes_path, notice: 'Prototype was successfully created.'
+    else
+      render :new
     end
   end
 
