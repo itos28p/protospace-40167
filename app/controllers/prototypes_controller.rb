fdab9cf1873c162aaed356500c24138323c61423
itos28p <rstoyo.sayaka.ito@gmail.com>
2023-12-22 22:52:15 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,6 +40,12 @@ class PrototypesController < ApplicationController
     redirect_to prototype_path
   end
 
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :prototypes  do
      resources :comments, only: :create
   end
-  resources :users, only: [:edit, :update, :show, :edit, :update]
+  resources :users, only: [:edit, :update, :show, :edit, :update, :destroy]
 
 end
