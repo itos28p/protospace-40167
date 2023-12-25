bc8bf991c9b3ec45b230e927010156d468928e81
itos28p <rstoyo.sayaka.ito@gmail.com>
2023-12-25 22:33:52 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     if @user
       @prototypes = @user.prototypes
+    else
+      flash[:alert] = "指定されたユーザーは存在しません。"
+      redirect_to root_path
     end
   end
 end
